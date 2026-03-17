ccfda68c556ae937412c7a41d9c37ff45c932aab
Ace667252 <tinamathew25@gmail.com>
2026-03-17 20:07:40 +0000

    Fix formatting and update quotes in vite.config.js

--- manga-bookshelf/vite.config.js
+++ manga-bookshelf/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/manga-bookshelf/',  // ← important for GitHub Pages
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/manga-bookshelf/"
+});
